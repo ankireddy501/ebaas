bd7c92062759559bfd68aad323627c1b52921ed8
ankireddy501 <reddy.501@gmail.com>
2015-05-04 11:19:16 +0530

    updated

--- src/main/java/com/ebaas/controller/AppUserAuthController.java
+++ src/main/java/com/ebaas/controller/AppUserAuthController.java
@@ -1,7 +1,28 @@
 package com.ebaas.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by anki on 04-05-2015.
  */
 public class AppUserAuthController {
+
+    @RequestMapping(value="/appUserAuth", produces = "application/json", method = RequestMethod.POST)
+    public @ResponseBody String authenticatePerson(@RequestBody String basicAuth) throws Exception {
+/*        Person person = personDAO.authenticate(userName, password);
+        if(person == null){
+            throw new Exception("Invalid Credentials");
+        }
+        String token = UUID.randomUUID().toString().toLowerCase();
+        SecurityContext securityContext = new SecurityContext();
+        securityContext.setTenantId(person.getTenantId());
+        securityContext.setPerson(person);
+        SecurityContextThreadLocal.set(securityContext);
+        SecurityContextProvider.addToken(token, securityContext);
+        return "{" +"\""+"Authorization"+"\""+":"+"\""+token+"\""+"}";*/
+        return null;
+    }
 }
